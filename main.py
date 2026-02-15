77e50ec2d9049c023c2fa84daeaafe4e1572d612
unknown <goodluckchile001@gmail.com>
2026-02-15 20:00:48 +0100

    news brodcast function update

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask
+import feedparser
 
+BBC_FEED = "https://feeds.bbci.co.uk/news/rss.xml"
 app = Flask(__name__)
 @app.route("/")
-def getNews():
-    return "no news is good"
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    firstArticle = feed["entries"][0]
+
+    return '''<html>
+    <body>
+        <h1> BBC Headlines </h1>
+        <b>{0}</b> <br/>
+        <i>{1}</i> <br/>
+        <p> {2}</p> <br/>
+    </body>
+     </html>'''.format(firstArticle.get("title"),firstArticle.get("published"),firstArticle.get("summary"))
 if __name__ == "__main__":
     app.run()

daa656977526658f142c33a25c70c53eb08035d5
unknown <goodluckchile001@gmail.com>
2026-02-28 09:30:49 +0100

    using GET

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import feedparser
 from flask import render_template
+from flask import request
+
+
 
 app = Flask(__name__)
 RSS_FEED ={'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -10,17 +13,15 @@ RSS_FEED ={'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 
 
-def get_news(publication):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEED[publication])
     return render_template("home.html",
                            articles = feed['entries'])
 
-@app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news("bbc")
-@app.route("/cnn")
-def cnn():
-    return get_news("cnn")
 if __name__ == "__main__":
     app.run()

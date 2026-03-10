0e9f0047f1ed85c9fd151429a085c95bfb9135fd
unknown <goodluckchile001@gmail.com>
2026-03-10 20:54:37 +0100

    bug fix

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 
 
-from flask import Flask
+from flask import Flask,render_template,request
 import feedparser
 import json
 import urllib.parse
 import urllib.request
-from flask import render_template
-from flask import request
+
 
 
 
@@ -15,11 +14,12 @@ DEFAULTS = {
     'publication':'bbc',
     'city':'Lagos'
 }
-RSS_FEED ={'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-           'cnn':'http://rss.cnn.com/rss/edition.rss',
-           'fox':'http://feeds.foxnews.com/foxnews/latest',
-           'iol':'http://www.iol.co.za/cmlink/1.640'}
+RSS_FEED ={'bbc': 'https://feeds.bbci.co.uk/news/rss.xml',
+           'cnn':'https://rss.cnn.com/rss/edition.rss',
+           'fox':'https://feeds.foxnews.com/foxnews/latest',
+           'iol':'https://www.iol.co.za/cmlink/1.640'}
 
+API_KEY= "907ca5c7ebf96a13e435f2ce904ecef3"
 
 
 @app.route("/")
@@ -31,6 +31,7 @@ def home():
     city = request.args.get("city")
     if not city:
         city = DEFAULTS['city']
+    city =city.strip()
     weather = get_weather(city)
     return render_template("home.html",articles=articles,weather=weather)
 def get_news(query):
@@ -42,10 +43,11 @@ def get_news(query):
 
     return feed["entries"]
 def get_weather(query):
-    api_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=907ca5c7ebf96a13e435f2ce904ecef3"
     query = urllib.parse.quote(query)
-    url = api_url.format(query)
-    data = urllib.request.urlopen(url).read()
+    api_url = f"https://api.openweathermap.org/data/2.5/weather?q={query}&units=metric&appid={API_KEY}"
+
+
+    data = urllib.request.urlopen(api_url).read()
     parsed = json.loads(data)
     weather = None
     if parsed.get("weather"):
@@ -56,4 +58,4 @@ def get_weather(query):
         }
     return weather
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
